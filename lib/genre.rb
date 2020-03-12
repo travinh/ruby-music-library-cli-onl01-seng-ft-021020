ac437af6819bd192db631af585c4bcc08ffc641b
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-12 05:54:45 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,41 +1,36 @@
-class Genre 
-  
+class Genre
   extend Concerns::Findable
-  
-  attr_accessor :name 
+  attr_accessor :name
   @@all = []
-  
+
   def initialize(name)
     @name = name
-    @songs = []
-    save
-    
-  end
-  
-  def save
-    @@all << self
-  end
-  
-  def self.destroy_all
-    @@all.clear
-  end
-  
+    @songs =[]
+  end 
+
   def self.all
     @@all
-  end
-  
+  end 
+
+  def self.destroy_all
+    @@all.clear
+  end 
+
+  def save
+    @@all << self
+  end 
+
   def self.create(name)
     genre = Genre.new(name)
-    genre.name = genre 
-    save
-  end
-  
-  def songs 
-    @songs 
-  end
-  
-  def artists
-    songs.collect {|song| song.artist}.uniq 
-  end
-  
+    genre.save
+    genre
+  end 
+
+  def songs
+    @songs
+  end 
+
+  def artists 
+    songs.map{|song| song.artist}.uniq
+  end 
 end
