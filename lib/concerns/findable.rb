818733183de670dab6d3eb13550933fee98fc4a5
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-12 05:49:00 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,13 +1,14 @@
 module Concerns::Findable
-  # Module code here
-  
-   def find_by_name(name)
-    self.all.detect {|song| song.name == name}
-  end
+  def find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end 
 
   def find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
-  end
+    if self.find_by_name(name)
+      return self.find_by_name(name)
+    else 
+      self.create(name)
+    end 
+  end 
 
-  
 end
